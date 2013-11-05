350f0ac715f5b9aaa27478960ab614e6789458ad
Ana Isabel <isagadala@gmail.com>
2013-11-05 20:32:10 -0300

    Updated frontend for the new authentication system
    Added simple_form
    Updated several forms to bootstrap/simple_form
    Added login/logout/register/profile links to our nav
    Added a collection select to our status form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,12 @@
 class ApplicationController < ActionController::Base
+  
+  before_filter :configure_devise_params, if: :devise_controller?
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  before_action :configure_devise_permitted_parameters, if: :devise_controller?
-
-  protected
-
   def configure_devise_permitted_parameters
-    registration_params = [:first_name, :last_name, :profile_name, :email, :password, :password_confirmation]
+    registration_params = [:email, :password, :password_confirmation, :first_name, :last_name, :profile_name]
 
     if params[:action] == 'update'
       devise_parameter_sanitizer.for(:account_update) { 
@@ -20,4 +18,12 @@ class ApplicationController < ActionController::Base
       }
     end
   end
-end
+
+
+  def configure_devise_params
+    devise_parameter_sanitizer.for(:sign_up) do |u|
+      u.permit(:email, :password, :password_confirmation, :first_name, :last_name, :profile_name)
+    end
+  end
+
+end 
